f50dba34d301adda06428e3fbd9b25d6598ff461
Arda Öngün <ardaongun3@gmail.com>
2024-11-20 04:52:56 +0300

    Add Menu

--- data-structures/linked_list/base_linked_list.c
+++ data-structures/linked_list/base_linked_list.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-#define STR_SIZE 100    
-
 
 typedef struct Node{
     int data;
@@ -79,6 +75,40 @@ void printList(Node* head){
     printf("NULL\n");
 }
 
-int main(){
+int main() {
     Node* head = NULL;
+    int choice, value;
+
+    while (1) {
+        printf("\nMenu:\n");
+        printf("1. Insert an element in sorted order\n");
+        printf("2. Delete an element\n");
+        printf("3. Print the list\n");
+        printf("4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+        case 1:
+            printf("Enter the value to insert: ");
+            scanf("%d", &value);
+            sortedInsert(&head, value);
+            break;
+        case 2:
+            printf("Enter the value to delete: ");
+            scanf("%d", &value);
+            deleteNode(&head, value);
+            break;
+        case 3:
+            printList(head);
+            break;
+        case 4:
+            printf("Exiting...\n");
+            exit(0);
+        default:
+            printf("Invalid choice. Please try again.\n");
+        }
+    }
+
+    return 0;
 }
